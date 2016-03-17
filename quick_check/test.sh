b9e0583ff225992cc62e172527fd9057ee35e092
David Thomas <d.thomas1@imperial.ac.uk>
2016-03-17 16:46:19 +0000

    Tidying up.

--- quick_check/test.sh
+++ quick_check/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 CODEGEN=../bin/c_codegen_ref
+
+# TODO: Make sure this points to your own mips gcc.
 MIPS_GCC=gcc
 
 (cd .. && make bin/c_codegen)
@@ -19,10 +21,13 @@ for i in input/*.c; do
 
   # TODO: C pre-processor?
   cat $i | $CODEGEN > build/$BASE.got.s
-
+  # Make sure this is set to a MIPS GCC above
   ${MIPS_GCC} driver.c build/$BASE.got.s -o build/$BASE.got
+  #This will work if QEMU is installed
   build/$BASE.got > output/$BASE.got
 
+  ##################################
+  ## Compare the output
   diff output/$BASE.got output/$BASE.ref > output/$BASE.diff;
   if [[ $? != 0 ]]; then
     echo "$BASE, FAIL, see output/$BASE.diff";
